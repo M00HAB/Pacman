b5a05a0488b2c2a799f6e8e9f5e206dd8c023cdc
Mohab Ashraf <me.hoba.012@gmail.com>
2024-02-03 18:02:11 +0200

    Initializing Repo

---
+++ Pacman/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.pacman {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.pacman to javafx.fxml;
+    exports com.example.pacman;
+}
